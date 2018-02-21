e9e68a8336fa5391fef41fefef8b98261e6c8485
piuy11 <34086332+piuy11@users.noreply.github.com>
2018-02-21 02:21:04 +0000

    parsed main

--- main.cpp
+++ main.cpp
@@ -2,9 +2,55 @@
 #include <string>
 #include <vector>
 #include <array>
+
 static constexpr int SudokuSize = 9;
+
+
 class SudokuBoard
-{ std::array<std::array<SudokuCell, SudokuSize>, SudokuSize> mCells; public: explicit SudokuBoard() {}  SudokuBoard& operator=(const std::array<std::string, SudokuSize>& input_) {  for (int i = 0; i < SudokuSize; i++) {   const std::string& str = input_[i];      for (int j = 0; j < SudokuSize; j++) {    mCells[i][j] = charToInt(str[j]);   }  } }};
-class SudokuCell{ // std::array<int, 9> temp; int mData;public: explicit SudokuCell(int data_) : mData(data_) {}  SudokuCell& operator=(int data_) {   }};
-inline int charToInt(char c){ return static_cast<int>(c - '0');}
-}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+{
+	std::array<std::array<SudokuCell, SudokuSize>, SudokuSize> mCells;
+  
+public:
+	explicit SudokuBoard() {}  
+	SudokuBoard& operator=(const std::array<std::string, SudokuSize>& input_)
+	{
+		for (int i = 0; i < SudokuSize; i++)
+		{
+			const std::string& str = input_[i];
+      
+			for (int j = 0; j < SudokuSize; j++)
+			{    
+				mCells[i][j] = charToInt(str[j]);
+			}
+		}
+	}
+};
+
+
+class SudokuCell{ 
+	// std::array<int, 9> temp; 
+	int mData;
+public:
+	explicit SudokuCell(int data_) : mData(data_) {}  
+	SudokuCell& operator=(int data_)
+	{
+	}
+};
+
+inline int charToInt(char c){ return static_cast<int>(c - '0'); }
+
+
+void main()
+{
+	SudokuBoard board;
+	std::cout << "input\n" << std::endl;
+	
+	for(int i = 0; i < SudokuSize; i++)
+	{
+		std::string input;
+		std::cin >> input;
+		if (input.length() != 9) {
+			std::cout << "ERROR : input again << std::endl;
+	
+	}
+}
